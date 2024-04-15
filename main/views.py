3638a76c2fd81bef7321be62f905c0e96b770b51
Gvzum <mirasgazimovv@icloud.com>
2024-04-16 00:38:14 +0600

    chore: first commit ever

---
+++ main/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    context = {
+        'title': 'Miras',
+        'content': 'главная страница магазина - HOME',
+        'list': ['first', 'second'],
+        'dict': {'first': 1},
+        'is_authenticated': False
+    }
+    
+    return render(request, 'main/index.html', context)
+    
+    
+def about(request):
+    return HttpResponse('About page')
